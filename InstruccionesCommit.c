edbc47fc2425aaf7c60041f4a62bf8cc61facd0d
Ibon Recio <ibon.recio@opendeusto.es>
2025-03-25 17:59:04 +0100

    commit

--- InstruccionesCommit.c
+++ InstruccionesCommit.c
@@ -2,3 +2,9 @@
 // git add .
 // git commit -m "commit"
 // git push origin main 
+
+/*
+git add file1.txt file2.txt
+git commit -m "Your descriptive commit message"
+git push origin branch_name
+*/
